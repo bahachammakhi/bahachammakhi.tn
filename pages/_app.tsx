b25831cf0f40472f491d31afca70589eb7b23696
Baha eddine chammakhi <bahachammakhi25@gmail.com>
2020-06-29 19:31:02 +0100

    Added Routing loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,21 @@ import React, { createContext, ContextType } from "react";
 import "antd/dist/antd.css";
 import dynamic from "next/dynamic";
 import useApi from "../hooks/useApi";
+import Preloader from "../components/ui/Preloader/Preloader";
+import NextNProgress from "nextjs-progressbar";
+const Layout = dynamic(
+  () => import("../components/layout/PortfolioLayout/Layout"),
+  {
+    ssr: false,
+    loading: () => <Preloader />,
+  }
+);
 import {
   getProjects,
   getSettings,
   postContact,
   getArticles,
+  getSocial,
 } from "../requests/requests";
 // import MessengerCustomerChat from "../utils/MessangerChat";
 const MessengerCustomerChat = dynamic(() => import("../utils/MessangerChat"), {
@@ -34,29 +44,39 @@ const MyApp = ({ Component, pageProps }: any): any => {
     getSettings,
     postContact,
     getArticles,
+    getSocial,
   });
 
   return (
     <Redux.Provider value={{ name: process.env.NAME, calls: calls }}>
-      <Component {...pageProps} />
-      <SEO />
-      <MessengerCustomerChat pageId="714175575683300" appId="272738577387387" />
-      <DefaultSeo {...DEFAULT_SEO} />
-      <style global jsx>
-        {`
-          body {
-            overflow-x: hidden;
-            @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
-            background: rgb(227, 228, 228);
-            background: linear-gradient(
-              90deg,
-              rgba(227, 228, 228, 0.6895133053221288) 0%,
-              rgba(255, 255, 255, 1) 98%
-            );
-            font-family: "Montserrat", sans-serif;
-          }
-        `}
-      </style>
+      <Layout>
+        <Component {...pageProps} />
+        <NextNProgress
+          color="#2a878f"
+          options={{ easing: "ease", speed: 500 }}
+        />
+        <SEO />
+        <MessengerCustomerChat
+          pageId="714175575683300"
+          appId="272738577387387"
+        />
+        <DefaultSeo {...DEFAULT_SEO} />
+        <style global jsx>
+          {`
+            body {
+              overflow-x: hidden;
+              @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+              background: rgb(227, 228, 228);
+              background: linear-gradient(
+                90deg,
+                rgba(227, 228, 228, 0.6895133053221288) 0%,
+                rgba(255, 255, 255, 1) 98%
+              );
+              font-family: "Montserrat", sans-serif;
+            }
+          `}
+        </style>
+      </Layout>
     </Redux.Provider>
   );
 };
